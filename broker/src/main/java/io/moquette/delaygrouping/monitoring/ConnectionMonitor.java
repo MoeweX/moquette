eab00251c9e9e209c234a2245e836d333fc4e1a1
Florian Stanek <flstanek@googlemail.com>
2019-06-26 20:53:50 +0200

    ConnectionMonitor now uses ping; Works and is so much easier :)

--- broker/src/main/java/io/moquette/delaygrouping/monitoring/ConnectionMonitor.java
+++ broker/src/main/java/io/moquette/delaygrouping/monitoring/ConnectionMonitor.java
@@ -4,76 +4,119 @@ import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.InetSocketAddress;
-import java.net.SocketException;
-import java.util.ArrayList;
-import java.util.concurrent.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ConnectionMonitor {
 
     private static final Logger LOG = LoggerFactory.getLogger(ConnectionMonitor.class);
-    private final LinkedBlockingQueue<InetSocketAddress> pingRequestQueue;
-    private final LinkedBlockingQueue<UDPListener.Measurement> measurementQueue;
+    private final int interval;
     private final int windowSize;
-    private ConcurrentHashMap<InetSocketAddress, DescriptiveStatistics> monitoredPeers = new ConcurrentHashMap<>();
-    private UDPListener udpListener;
-    private ExecutorService resultExecutor = Executors.newSingleThreadExecutor();
+    private final String pingRegex = "icmp_seq=\\d+ ttl=\\d+ time=([\\d\\.]+) ms";
+    private final Pattern pingPattern = Pattern.compile(pingRegex, Pattern.MULTILINE);
+    private ConcurrentHashMap<InetAddress, DescriptiveStatistics> monitoredPeers = new ConcurrentHashMap<>();
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
+    private Map<InetAddress, Process> runningInstances = new ConcurrentHashMap<>();
+    private ProcessBuilder processBuilder = new ProcessBuilder();
 
-    public ConnectionMonitor(int listenPort, ScheduledExecutorService executorService, int interval, int windowSize) {
+    public ConnectionMonitor(int interval, int windowSize) {
+        this.interval = interval;
         this.windowSize = windowSize;
 
         LOG.info("Starting connection monitor...");
 
-        pingRequestQueue = new LinkedBlockingQueue<>();
-        measurementQueue = new LinkedBlockingQueue<>();
-
-        try {
-            udpListener = new UDPListener(listenPort, pingRequestQueue, measurementQueue);
-            udpListener.start();
-            LOG.info("Connection monitor started (ping interval = {}ms). Listening on port {}", interval, listenPort);
-        } catch (SocketException e) {
-            LOG.error("Error starting listener on port {}: {}", listenPort, e);
-        }
-
-        executorService.scheduleAtFixedRate(() -> {
-            // Send ping requests
-            monitoredPeers.forEach((address, stats) -> {
-                pingRequestQueue.add(address);
-            });
-
-            // Trigger result collection
-            collectResults();
-        }, 0, interval, TimeUnit.MILLISECONDS);
-
+        // maybe add some cyclic result collection...
     }
 
-    private void collectResults() {
-        resultExecutor.execute(() -> {
-            ArrayList<UDPListener.Measurement> newResults = new ArrayList<>();
-            measurementQueue.drainTo(newResults);
+    private void collectResults(InetAddress address) {
+        // only call from executor!
+        // read and parse results from all running instances
+        Process instance = runningInstances.get(address);
+        if (instance == null) return;
 
-            newResults.forEach(measurement -> {
-                monitoredPeers.get(measurement.peerAddress).addValue(measurement.delayValue);
-            });
+        InputStream pingStdOut = instance.getInputStream();
+        StringBuilder accumulator = new StringBuilder();
+        char character;
+
+        try {
+            while (pingStdOut.available() > 0) {
+                character = (char) pingStdOut.read();
+                if (character == 10) {
+                    pingStdOut.mark(100);
+                }
+                accumulator.append(character);
+            }
+            pingStdOut.reset();
+        } catch (IOException ex) {
+            LOG.error("IOException while reading stdOut: {}", ex);
+        }
+
+        List<String> outputLines = Arrays.asList(accumulator.toString().split("\n"));
+        DescriptiveStatistics peerStats = monitoredPeers.get(address);
+
+        outputLines.forEach(line -> {
+            Matcher matcher = pingPattern.matcher(line);
+            if (matcher.find()) {
+                String delay = matcher.group(1);
+                try {
+                    peerStats.addValue(Double.valueOf(delay));
+                } catch (NumberFormatException ignored) {
+                }
+            }
         });
     }
 
     public void shutdown() {
-        udpListener.shutdown();
+        // kill all running ping instances
+        runningInstances.forEach((address, process) -> process.destroy());
     }
 
-    public void addMonitoredPeer(InetSocketAddress address) {
-        monitoredPeers.put(address, new DescriptiveStatistics(windowSize));
-        LOG.info("STARTED monitoring peer at {}", address);
+    public void addMonitoredPeer(InetAddress address) {
+        if (monitoredPeers.containsKey(address)) return;
+
+        Process instance = createProcess(address);
+        if (instance != null) {
+            monitoredPeers.put(address, new DescriptiveStatistics(windowSize));
+            runningInstances.put(address, instance);
+
+            LOG.info("STARTED monitoring peer at {}", address);
+        }
+
     }
 
-    public void removeMonitoredPeer(InetSocketAddress address) {
+    public void removeMonitoredPeer(InetAddress address) {
         monitoredPeers.remove(address);
+        Process instance = runningInstances.remove(address);
+        if (instance != null) {
+            instance.destroy();
+        }
+
         LOG.info("STOPPED monitoring peer at {}", address);
     }
 
-    public Future<Double> getAverageDelay(InetSocketAddress address) {
-        return resultExecutor.submit(() -> {
+    private Process createProcess(InetAddress address) {
+        processBuilder.command("ping", "-n", "-i", String.valueOf(interval / 1000), address.getHostName());
+        try {
+            return processBuilder.start();
+        } catch (IOException e) {
+            LOG.error("Error creating ping instance: {}", e);
+            return null;
+        }
+    }
+
+    public Future<Double> getAverageDelay(InetAddress address) {
+        return executor.submit(() -> {
+            collectResults(address);
             DescriptiveStatistics stats = monitoredPeers.get(address);
             if (stats != null) {
                 return (stats.getMean() / 2) / 1000;
@@ -83,8 +126,9 @@ public class ConnectionMonitor {
         });
     }
 
-    public Future<DescriptiveStatistics> getStats(InetSocketAddress address) {
-        return resultExecutor.submit(() -> {
+    public Future<DescriptiveStatistics> getStats(InetAddress address) {
+        return executor.submit(() -> {
+            collectResults(address);
             DescriptiveStatistics stats = monitoredPeers.get(address);
             if (stats != null) {
                 return stats.copy();
--- broker/src/test/java/io/moquette/integration/ConnectionMonitorTest.java
+++ broker/src/test/java/io/moquette/integration/ConnectionMonitorTest.java
@@ -2,38 +2,84 @@ package io.moquette.integration;
 
 import io.moquette.delaygrouping.monitoring.ConnectionMonitor;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.junit.Ignore;
 import org.junit.Test;
 
-import java.net.InetSocketAddress;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConnectionMonitorTest {
 
     @Test
-    public void testPingPong() throws ExecutionException, InterruptedException {
-        ConnectionMonitor monitor1 = new ConnectionMonitor(1885, Executors.newScheduledThreadPool(1), 1000, 20);
-        ConnectionMonitor monitor2 = new ConnectionMonitor(2885, Executors.newScheduledThreadPool(1), 1000, 20);
+    @Ignore
+    public void testManyPings() throws ExecutionException, InterruptedException, UnknownHostException {
+        ConnectionMonitor monitor1 = new ConnectionMonitor(1000, 20);
 
-        monitor1.addMonitoredPeer(new InetSocketAddress("localhost", 2885));
-        monitor2.addMonitoredPeer(new InetSocketAddress("localhost", 1885));
-
-        while (true) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            DescriptiveStatistics stats1 = monitor1.getStats(new InetSocketAddress("localhost", 2885)).get();
-            DescriptiveStatistics stats2 = monitor2.getStats(new InetSocketAddress("localhost", 1885)).get();
-            System.out.println(stats1.getMean());
-            System.out.println(Arrays.toString(stats1.getValues()));
-            System.out.println(stats2.getMean());
-            System.out.println(Arrays.toString(stats2.getValues()));
-
+        for (int i = 1; i < 250; i++) {
+            monitor1.addMonitoredPeer(InetAddress.getByName("127.0.0." + i));
         }
 
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        DescriptiveStatistics stats1 = monitor1.getStats(InetAddress.getByName("127.0.0.1")).get();
+        System.out.println(stats1.getMean());
+        System.out.println(Arrays.toString(stats1.getValues()));
+    }
+
+    @Test
+    public void testPing() throws ExecutionException, InterruptedException, UnknownHostException {
+        ConnectionMonitor monitor1 = new ConnectionMonitor(1000, 20);
+
+        monitor1.addMonitoredPeer(InetAddress.getByName("127.0.0.1"));
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        DescriptiveStatistics stats1 = monitor1.getStats(InetAddress.getByName("127.0.0.1")).get();
+        assertThat(stats1).isNotNull();
+        assertThat(stats1.getValues()).isNotEmpty();
+    }
+
+    @Test
+    public void testPingAddAndRemove() throws ExecutionException, InterruptedException, UnknownHostException {
+        ConnectionMonitor monitor1 = new ConnectionMonitor(1000, 20);
+
+        monitor1.addMonitoredPeer(InetAddress.getByName("127.0.0.1"));
+        monitor1.addMonitoredPeer(InetAddress.getByName("127.0.0.2"));
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        DescriptiveStatistics stats1 = monitor1.getStats(InetAddress.getByName("127.0.0.1")).get();
+        assertThat(stats1).isNotNull();
+        assertThat(stats1.getValues()).isNotEmpty();
+
+        DescriptiveStatistics stats2 = monitor1.getStats(InetAddress.getByName("127.0.0.2")).get();
+        assertThat(stats2).isNotNull();
+        assertThat(stats2.getValues()).isNotEmpty();
+
+        monitor1.removeMonitoredPeer(InetAddress.getByName("127.0.0.1"));
+
+        stats1 = monitor1.getStats(InetAddress.getByName("127.0.0.1")).get();
+        assertThat(stats1).isNull();
+
+        stats2 = monitor1.getStats(InetAddress.getByName("127.0.0.2")).get();
+        assertThat(stats2).isNotNull();
+        assertThat(stats2.getValues()).isNotEmpty();
     }
 }
 
