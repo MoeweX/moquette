c037cece582c1584458cf7984d583eb67b4e6450
Florian Stanek <flstanek@googlemail.com>
2019-07-11 17:18:43 +0200

    Fix memory leak

--- broker/src/main/java/io/moquette/delaygrouping/anchor/MessageStore.java
+++ broker/src/main/java/io/moquette/delaygrouping/anchor/MessageStore.java
@@ -1,17 +1,21 @@
 package io.moquette.delaygrouping.anchor;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class MessageStore {
-    private Map<String, Integer> hashToCount = new ConcurrentHashMap<>();
+    private Map<ByteBuffer, Integer> hashToCount = new ConcurrentHashMap<>();
 
     public void saveMessage(String topic, String payload) {
         save(topic + payload);
     }
 
     public void save(String value) {
-        hashToCount.compute(value, (key, oldValue) -> {
+        hashToCount.compute(getDigest(value), (key, oldValue) -> {
             if (oldValue == null) {
                 return 1;
             } else {
@@ -21,7 +25,7 @@ public class MessageStore {
     }
 
     public boolean remove(String value) {
-        var result = hashToCount.computeIfPresent(value, (key, oldValue) -> {
+        var result = hashToCount.computeIfPresent(getDigest(value), (key, oldValue) -> {
             if (oldValue == 0) {
                 return null;
             } else {
@@ -29,5 +33,14 @@ public class MessageStore {
             }
         });
         return result != null;
+    }
+
+    private ByteBuffer getDigest(String value) {
+        try {
+            return ByteBuffer.wrap(MessageDigest.getInstance("SHA-256").digest(value.getBytes(StandardCharsets.UTF_8)));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
